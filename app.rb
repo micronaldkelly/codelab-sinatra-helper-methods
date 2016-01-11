2c8527ef41e015f808f47d27a320ba95a2936c86
micronaldkelly <kellym15@msu.edu>
2016-01-11 18:53:35 -0500

    sinatra helper methods

---
+++ app.rb
@@ -0,0 +1,51 @@
+require 'sinatra'
+class MyWebApp < Sinatra::Base
+  # get '/' do
+  #  "You are a Pawn in this game, #{Time.now.year}"
+  #end
+  get '/' do
+    erb :generator
+  end
+  get '/about' do
+    erb :about
+  end
+  get '/story' do
+    erb :story
+  end
+  helpers do
+    def current_year
+      Time.now.year
+    end
+    def current_date(country)
+      if country == 'usa' || country == 'canada'
+        current_date_mdy
+      else
+        current_date_dmy
+      end
+    end
+
+    def current_date_mdy
+      current_time = Time.now
+      "#{ month.day }/#{ current_time.day }/#{ current_time.year }"
+    end
+
+    def current_date_dmy
+      current_time = Time.now
+      "#{ current_time.day }/#{ current_time.month }/#{ current_time.year }"
+    end
+    def current_date(country)
+      if country.casecmp('usa') == 0 || country.casecmp('canada') == 0
+        current_date_mdy
+      else
+        current_date_dmy
+      end
+    end
+    def pluralize(number, text)
+      if number.to_f != 1
+        %(#{text}s)
+      else
+        text
+      end
+    end
+  end
+end
